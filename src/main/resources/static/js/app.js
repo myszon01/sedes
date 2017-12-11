69b3b4834e8c52f7f602903c3725724c91700dbd
myszon01 <myszon01@gmail.com>
2017-12-10 23:37:43 -0800

    up

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,4 +1,7 @@
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import toastr from 'toastr';
 
 var Customer = React.createClass({
 	
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,22 @@
-var path = require('path');
+
 
 module.exports = {
-    entry: './src/main/js/app.js',
-    devtool: 'sourcemaps',
-    cache: true,
-    debug: true,
-    output: {
-        path: __dirname,
-        filename: './src/main/resources/static/built/bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: path.join(__dirname, '.'),
-                exclude: /(node_modules)/,
-                loader: 'babel',
-                query: {
-                    cacheDirectory: true,
-                    presets: ['es2015', 'react']
-                }
-            }
-        ]
-    }
-};
+  entry: './src/main/resources/static/js/app.js',
+  output: {
+    path: __dirname + '/src/main/webapp/public', 
+    filename: 'bundle.js' 
+  },
+  module: {
+	    loaders: [
+	      {
+	        test: /.js$/,
+	        loader: 'babel',
+	        exclude: /node_modules/,
+	        query: {
+	          presets: ['es2015', 'react']
+	        }
+	      }
+	    ]
+	  }
+	  
+	}
